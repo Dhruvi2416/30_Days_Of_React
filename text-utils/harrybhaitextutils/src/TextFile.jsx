f98697c9a765e79131dd3e977d7b2f0799f2efae
DhruviShah16 <19dce123@charusat.edu.in>
2023-08-26 12:16:44 +0530

    text

--- text-utils/harrybhaitextutils/src/TextFile.jsx
+++ text-utils/harrybhaitextutils/src/TextFile.jsx
@@ -4,6 +4,7 @@ const TextFile = (props) => {
   const [text, setText] = useState("");
   const [word, setWord] = useState("");
   const [wordFound, setWordFound] = useState(false);
+  //if word matches then it says matched
   const handleFind = () => {
     console.log();
     if (text.includes(word)) {
@@ -13,7 +14,11 @@ const TextFile = (props) => {
     }
     setTimeout(()=>setWordFound(false),2000)
   };
-  console.log(text.includes(word));
+  //copies text
+  const handleCopy =()=>{
+    navigator.clipboard.writeText(text)
+  }
+ 
   return (
     <div className="container pt-5 d-flex flex-column justify-content-center">
       <h1 className="d-flex justify-content-center">{props.title}</h1>
@@ -26,44 +31,49 @@ const TextFile = (props) => {
         value={text}
         onChange={(e) => setText(e.target.value)}
       ></textarea>
-      <div className="d-flex justify-content-center">
+      <div className="d-flex flex-wrap justify-content-center">
         <button
-          className="btn btn-primary mr-3 my-2"
+          className="btn btn-primary mt-2 mx-1  sm:my-4 mx-2 lg:m-3"
           onClick={() => setText(text.toUpperCase())}
         >
           Convert to Uppercase
         </button>
         <button
-          className="btn btn-primary m-3 my-2"
+          className="btn btn-primary mt-2 mx-1  sm:my-4 mx-2 lg:m-3"
           onClick={() => setText(text.toLowerCase())}
         >
           {" "}
           Convert to Lowercase
         </button>
         <button
-          className="btn btn-primary mr-5 my-2"
+          className="btn btn-primary  mt-2 mx-1  sm:my-4 mx-2 lg:m-3"
+          onClick={() => handleCopy()}
+        >
+          Copy Text
+        </button>
+        <button
+          className="btn btn-primary mt-2 mx-1  sm:my-4  mx-2 lg:m-3 "
           onClick={() => setText("")}
         >
           Clear Text
         </button>
-      </div>
-      <div className="d-flex justify-content-center">
+       <div className="d-flex">
         <input
-          className="my-4"
+          className="mt-2 mx-1  sm:my-4 mx-2 lg:m-3"
           placeholder="Find word"
           onChange={(e) => setWord(e.target.value)}
           value={word}
         ></input>
-        <button className="btn btn-primary m-3 " onClick={() => handleFind()}>
+        <button className="btn btn-primary mt-2 mx-1  sm:my-4 mx-2 lg:m-3 " onClick={() => handleFind()}>
           Word Found?{" "}
-        </button>
+        </button></div>
       </div>
       <div>
         <h3 className="mt-2">Your text summary</h3>
-        {text.split("").length > 0 ? text.split(/\s+/).length : 0} words{" "}
+        {text.split("").length > 0 ? text.trim().split(" ").length : 0} words{" "}
         {text.split("").length} characters
       </div>
-      Time read: {((0.4/60) * text.split(" ").length).toFixed(2)} Minutes read
+      Time read: {(text.split("").length > 0 ?(0.4/60) * text.split(" ").length:0.00).toFixed(2)} Minutes read
       <h2 className="mt-5 text-primary text-opacity-50">Preview</h2>
       {text}
     </div>
